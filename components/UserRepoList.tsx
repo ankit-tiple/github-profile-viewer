66e8b2dcb7e3b6f9cd541f8ec0099d0799928d01
Ankit Tiple <ankittiple97@gmail.com>
2026-07-12 23:24:23 +0530

    Added the repo Input

--- components/UserRepoList.tsx
+++ components/UserRepoList.tsx
@@ -1,7 +1,18 @@
 "use client";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import UserRepo from "@/types/user-repo";
 export default function UserRepoList({ repo }: any) {
+  const [repoArray, setRepoArray] = useState(repo);
+  const [repoName, setRepoName] = useState("");
+
+  const inputChangeHandler = (event) => {
+    setRepoName(event.target.value);
+    const resultArray = repo.filter((ele) => {
+      return ele[1].name.toLowerCase().includes(repoName.toLowerCase());
+    });
+    setRepoArray([...resultArray]);
+  };
+
   // As the Date.now() is Impure function React doesn't allow it's use as it may provide inconsistent output
   // impure render = unerilable output ont he render that occurs
   const now = useMemo(() => Date.now(), []);
@@ -21,12 +32,6 @@ export default function UserRepoList({ repo }: any) {
     for (const [unitKey, unitValue] of units) {
       if (diffInSec >= unitValue) {
         const getActualValue = Math.floor(diffInSec / unitValue);
-        console.log(
-          new Intl.RelativeTimeFormat("en", { numeric: "auto" }).format(
-            -getActualValue,
-            unitKey,
-          ),
-        );
         //  console.log(
         //    `The diffInSec ${diffInSec} and unitValue ${unitValue} and the getActualValue is ${getActualValue}`,
         //  );
@@ -40,9 +45,15 @@ export default function UserRepoList({ repo }: any) {
   getUpdatedAtDate("2026-06-27T11:52:48Z", now);
   return (
     <div>
-      {`Public Repostitories ${repo.length}`}
+      <input
+        placeholder="Search respositories..."
+        value={repoName}
+        onChange={(e) => inputChangeHandler(e)}
+      />
+      <br />
+      {`Public Repostitories ${repoArray.length}`}
       <div>
-        {repo.map((ele: any, id: string) => {
+        {repoArray.map((ele: any, id: string) => {
           const [
             name,
             description,
@@ -50,6 +61,7 @@ export default function UserRepoList({ repo }: any) {
             updated_at,
             stargazers_count,
             fork_count,
+            open_issues,
           ] = [
             ele[1].name,
             ele[1].description,
@@ -57,6 +69,7 @@ export default function UserRepoList({ repo }: any) {
             ele[1].updated_at,
             ele[1].stargazers_count,
             ele[1].fork_count,
+            ele[1].open_issues,
           ];
 
           return (
@@ -65,6 +78,9 @@ export default function UserRepoList({ repo }: any) {
               <p>{description}</p>
               <h3>{language}</h3>
               <h4>{`Updated ${getUpdatedAtDate(updated_at, now)}`}</h4>
+              <h4>{`Stars ${stargazers_count}`}</h4>
+              {fork_count && <h4>{`fork ${fork_count}`}</h4>}
+              <h4>{`Issues ${open_issues}`}</h4>
               <br></br>
             </div>
           );
